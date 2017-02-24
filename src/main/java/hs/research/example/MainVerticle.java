f6a41dda7a8e328bc2c4c12ffbe24f9ef464997f
Henrique Sakata <henriquesakata@gmail.com>
2017-02-23 23:22:31 -0300

    Setup inicial.

---
+++ src/main/java/hs/research/example/MainVerticle.java
@@ -0,0 +1,11 @@
+package hs.research.example;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+    }
+
+}
