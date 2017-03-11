4ae98663ef745df540d74f46a0181bb02fc29247
Henrique Sakata <henriquesakata@gmail.com>
2017-03-10 23:24:13 -0300

    Instrução básica

--- README.md
+++ README.md
@@ -5,9 +5,26 @@
 - Java 8
 - NodeJS
 - Docker
+- Maven
 
 ## Setup
 
-### Install Keycloak
+### Install Keycloak on Docker
 
+To start Keycloak on Docker simply run 
+
+```sh
 docker run -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -p 9080:8080 jboss/keycloak
+```
+
+This will expose Keycloak on localhost:9080. If you want to expose Keycloak on a different port replace the first 9080 with the port you want to expose it on. 
+
+You should now have Keycloak server up and running. To check that it's working open http://DOCKER-IP:9080/auth or http://localhost:9080/auth if you exposed it on localhost with -p. Then click on Admin Console the username is admin and password is admin. 
+
+Create new Realm and upload rockt-realm.json, then click on Create button.
+
+### Build project
+
+```sh
+mvn clean install -P frontend
+```
--- src/main/java/hs/research/example/MainVerticle.java
+++ src/main/java/hs/research/example/MainVerticle.java
@@ -14,32 +14,11 @@ public class MainVerticle extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         final Router router = Router.router(vertx);
-        // router.get("/api/time").handler(this::currentTime);
-        // router.route("/*").handler(StaticHandler.create());
-        // final HttpServerOptions options = new HttpServerOptions().setCompressionSupported(true);
-        // HttpServer httpServer = vertx.createHttpServer(options);
-        // httpServer.requestHandler(router::accept).listen(8080, "0.0.0.0", asyncResult -> {
-        // if (asyncResult.succeeded()) {
-        // System.out.println("Server started on port " + asyncResult.result().actualPort());
-        // } else {
-        // asyncResult.cause().printStackTrace();
-        // }
-        // });
         configureSecurity(router);
         router.route("/*").handler(StaticHandler.create());
         router.mountSubRouter("/api/services", new RestServices(vertx).getRouter());
         configureAndStartServer(router);
     }
-
-    // private void currentTime(final RoutingContext routingContext) {
-    // JsonObject jsonObject = new JsonObject();
-    // jsonObject.put("current_time", new Date().toString());
-    // routingContext.response()
-    // .setStatusCode(200)
-    // .putHeader("content-type", "application/json; charset=utf-8")
-    // .putHeader("cache-control", "no-cache, must-revalidate")
-    // .end(jsonObject.encode());
-    // }
 
     private void configureAndStartServer(final Router router) {
         final HttpServerOptions options = new HttpServerOptions().setCompressionSupported(true);
